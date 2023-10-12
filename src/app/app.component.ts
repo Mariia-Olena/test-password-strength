2f871864e92c86efb75af8f2de19ec75f5258a4c
Mariia-Olena <mariia.stus@gmail.com>
2023-10-12 08:47:54 +0300

    simplify template and move logic to the component class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,20 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  message = '';
+
   form = new FormGroup({
     input: new FormControl('', [
       Validators.required,
       Validators.minLength(8),
+      Validators.pattern(/^[a-zA-Z0-9/!@#$%^&*()_+\-=\[\]{};\':"\\|,.<>\/?]+$/),
       this.passwordStrengthValidator(),
     ]),
   });
+
+  get input(): FormControl {
+    return this.form.controls.input;
+  }
 
   passwordStrengthValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
@@ -31,9 +38,7 @@ export class AppComponent {
       }
 
       const hasLetters = /[A-Za-z]/.test(value);
-
       const hasNumbers = /\d/.test(value);
-
       const hasSymbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(value);
 
       const passwordValid = hasLetters && hasNumbers && hasSymbols;
@@ -42,13 +47,13 @@ export class AppComponent {
     };
   }
 
-  checkCombination(number: number): boolean {
+  checkPasswordCombination(number: number): boolean {
     if (this.form.controls.input.errors) {
       const { hasLetters, hasNumbers, hasSymbols } =
         this.form.controls.input.errors;
 
       const arr = [hasLetters, hasNumbers, hasSymbols].filter(
-        (item) => item === true,
+        (item) => item === true
       );
 
       return arr.length === number ? true : false;
@@ -57,19 +62,52 @@ export class AppComponent {
     return false;
   }
 
-  minLength(): boolean {
-    return this.form.controls.input.errors?.['minlength'];
+  passwordMinlengthError() {
+    return this.input.errors?.['minlength'];
   }
 
-  easy(): boolean {
-    return this.checkCombination(1);
+  passwordPatternError() {
+    return this.input.errors?.['pattern'];
   }
 
-  medium(): boolean {
-    return this.checkCombination(2);
+  isPasswordEasy(): boolean {
+    return this.checkPasswordCombination(1);
   }
 
-  strong(): boolean {
+  isPasswordMedium(): boolean {
+    return this.checkPasswordCombination(2);
+  }
+
+  isPasswordStrong(): boolean {
     return this.form.controls.input.errors === null;
+  }
+
+  checkPasswordStrength(): string {
+    if (this.passwordPatternError()) {
+      this.message = 'Cyrillic letters and spaces are not allowed';
+      return 'invalid';
+    }
+
+    if (this.passwordMinlengthError()) {
+      this.message = 'Password should be at least 8 characters long';
+      return 'invalid';
+    }
+
+    if (this.isPasswordStrong()) {
+      this.message = 'Password is strong';
+      return 'strong';
+    }
+
+    if (this.isPasswordMedium()) {
+      this.message = 'Password is medium';
+      return 'medium';
+    }
+
+    if (this.isPasswordEasy()) {
+      this.message = 'Password is easy';
+      return 'easy';
+    }
+
+    return '';
   }
 }
